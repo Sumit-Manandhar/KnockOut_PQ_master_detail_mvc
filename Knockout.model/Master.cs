3c20c3b889446ae1816650a833418856476c4827
kreamBell <sumit.mdr999@gmail.com>
2022-03-14 16:53:24 +0545

    Add project files.

---
+++ Knockout.model/Master.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Knockout.model
+{
+    public class Master
+    {
+
+        public int Id { get; set; }
+        public String FName { get; set; }
+        public String MName { get; set; }
+        public String LName { get; set; }
+        public String Email { get; set; }
+        public String Phone { get; set; }
+        public String Citizenship { get; set; }
+
+        public List<Details> DetailsList { get; set; }
+    }
+}
---
+++ Knouckout.core/Context.cs
@@ -0,0 +1,26 @@
+﻿using Knockout.model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Knouckout.core
+{
+    class Context: DbContext
+    {
+        public Context() : base("DefaultConnection")
+        {
+
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+        public DbSet<Master> Master { get; set; }
+        public DbSet<Details> Details { get; set; }
+    }
+}
